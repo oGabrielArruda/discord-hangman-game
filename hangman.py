db054d660ff3f6d34992ce1e51ad8dfb20469636
oGabrielArruda <47468017+oGabrielArruda@users.noreply.github.com>
2021-04-08 22:16:26 -0300

    clue and letter attempt

--- hangman.py
+++ hangman.py
@@ -6,8 +6,13 @@ class Hangman:
 	def __init__(self):
 		self.__challenge_object = random.choice(challenges)
 		
-		length = len(self.word_object["word"])
-		self.__answer = "- " * length
+		word_length = len(self.__challenge_object["word"])
+		
+		self.__answer = "- " * word_length
+
+		self.__letters_left = word_length
+		
+		self.__errors_left = int(word_length * 0.3)
 
 
 	@property
@@ -19,10 +24,26 @@ class Hangman:
 		self.__answer = state
 	
 	@property
-	def word_object(self):
+	def challenge_object(self):
 		return self.__challenge_object
 
-	@word_object.setter
-	def word_object(self, state):
+	@challenge_object.setter
+	def challenge_object(self, state):
 		self.__challenge_object["word"] = state["word"]
 		self.__challenge_object = state["clue"]
+
+	@property
+	def letters_left(self):
+		return self.__letters_left
+
+	@letters_left.setter
+	def letters_left(self, state):
+		self.__letters_left = state
+
+	@property
+	def errors_left(self):
+		return self.__errors_left
+
+	@errors_left.setter
+	def errors(self, state):
+		self.__errors_left = state
--- main.py
+++ main.py
@@ -18,15 +18,30 @@ games_on = {}
 async def start_game(ctx):
 	player_id = ctx.__dict__['message'].author.id
 	player_mention = ctx.__dict__['message'].author.mention
-	
+
 	game = Hangman()
 	games_on[player_id] = { "mention": player_mention, "game": game }
 
 	await ctx.send(f'{player_mention} the game has started!\n')
-	await ctx.send(game.answer)	
 	
-		
+	await ctx.send(game.answer + f"\nThere is {game.letters_left} letters left!")
 
+@bot.command(name='clue', help="Attempt of letter")
+async def clue(ctx):
+	player_id = ctx.__dict__['message'].author.id
+	if not games_on.__contains__(player_id):
+		await ctx.send("The game isn't started yet! Type !start to begin.")
+	
+	clue = games_on[player_id]["game"].challenge_object["clue"]
+	await ctx.send(f"The clue is: '{clue}'")
+
+@bot.command(name='letter', help="Attempt of letter")
+async def letter_attempt(ctx, letter : str):
+	player_id = ctx.__dict__['message'].author.id
+	if not games_on.__contains__(player_id):
+		await ctx.send("The game isn't started yet! Type !start to begin.")
+
+	print(letter)
 
 
 bot.run(TOKEN)
